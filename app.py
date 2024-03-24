79a1d9c5ed951ef1298decc65664ee1cf691fd20
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-03-24 06:13:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,15 @@
-from diffusers import DiffusionPipeline
-import torch
-from io import BytesIO
-import base64
-from huggingface_hub import snapshot_download
-import os
+import json
+from sentence_transformers import SentenceTransformer, util
 
 class InferlessPythonModel:
     def initialize(self):
-        self.pipe = DiffusionPipeline.from_pretrained(
-            "playgroundai/playground-v2.5-1024px-aesthetic",
-            use_safetensors=True,
-            torch_dtype=torch.float16,
-            variant="fp16"
-        ).to("cuda")
+        self.model = SentenceTransformer("jinaai/jina-embeddings-v2-base-en",trust_remote_code=True)
+        # control your input sequence length up to 8192
+        self.model.max_seq_length = 1024
 
     def infer(self, inputs):
-        prompt = inputs["prompt"]
-        image = self.pipe(prompt, num_inference_steps=50, guidance_scale=3).images[0]
-        buff = BytesIO()
-        image.save(buff, format="JPEG")
-        img_str = base64.b64encode(buff.getvalue()).decode()
-        return { "generated_image_base64" : img_str }
-        
-    def finalize(self):
+        sentences = inputs["sentences"]
+        embeddings = self.model.encode(sentences)
+        return {"result": embeddings}
+    def finalize(self, args):
         self.pipe = None
