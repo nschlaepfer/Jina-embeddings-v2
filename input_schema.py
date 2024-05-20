86fc286a6eb3129ec01a0e8552090acff422cb0c
nschlaepfer <44988633+nschlaepfer@users.noreply.github.com>
2024-05-20 15:30:15 +0100

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -2,7 +2,7 @@ INPUT_SCHEMA = {
     "sentences": {
         'datatype': 'STRING',
         'required': True,
-        'shape': [1],
+        'shape': [-1],
         'example': ["What is Deep Learning?"]
     }
 }
